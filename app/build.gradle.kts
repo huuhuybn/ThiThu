41bd81e8b5eede5147e1b7b59e458dab448b40a2
huynh13 <80864294+huynh13@users.noreply.github.com>
2024-06-08 20:12:49 +0700

    Update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ android {
     }
     buildFeatures {
         compose = true
+        viewBinding = true
     }
     composeOptions {
         kotlinCompilerExtensionVersion = "1.4.3"
@@ -48,8 +49,9 @@ android {
         }
     }
 }
-
 dependencies {
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
     implementation("io.coil-kt:coil-compose:2.6.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
@@ -65,6 +67,9 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.activity:activity:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/vn/poly/mob305/myapplication/MainActivity.kt
+++ app/src/main/java/vn/poly/mob305/myapplication/MainActivity.kt
@@ -1,95 +1,117 @@
 package vn.poly.mob305.myapplication
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
+import androidx.compose.ui.window.Dialog
 
 class MainActivity : ComponentActivity() {
-    // remember : hoc roi
-    // từ khóa để lưu trạng thái (state) của 1 biến
-    // khi state thay đổi thì giao diện cũng được cập nhật giá trị mới
-    // rememberSavable
-    // navigation : chuyển hướng màn hình != startActivity
-    // navigation ~ fragment : chuyển đổi giao diện componsable
-    // ViewModel
-    // MVVM
+    // LazyColum là 1 compose hiển thị danh sách từ mảng - List - Array ...
+
+    data class Cat(val id: Int, val name: String, val des: String, val url: String)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // tạo 1 array các loài mèo
+        var catList = listOf(
+            Cat(
+                1, "MEOMEO",
+                "Meo rat quy hiem",
+                "https://cdn.britannica.com/70/234870-050-D4D024BB/Orange-colored-cat-yawns-displaying-teeth.jpg"
+            ),
+            Cat(
+                2, "MeoBeo",
+                "Meo rat quy hiem, nhung hoi luoi an",
+                "https://cdn.britannica.com/34/235834-050-C5843610/two-different-breeds-of-cats-side-by-side-outdoors-in-the-garden.jpg"
+            ), Cat(
+                3, "MEOMEO",
+                "Meo rat quy hiem, nhung rat hay an",
+                "https://www.animalfriends.co.uk/siteassets/media/images/article-images/cat-articles/51_afi_article1_the-secret-language-of-cats.png"
+            ), Cat(
+                4, "<MEOMEO>123",
+                "Meo rat quy hiem",
+                "https://cdn.britannica.com/25/172925-050-DC7E2298/black-cat-back.jpg"
+            )
+        )
+
         setContent {
-            // getValue - setValue 
-//            var count: Int by remember { mutableStateOf(0) }
-//            Button(onClick = { count++ }) {
-//                Text(text = "Dem $count", fontSize = 44.sp)
-//            }
-            // su dung giong remember : saveable : lưu trạng thái lâu hơn so với remember
-            // cụ thể : khi ứng dụng đổi language hoặc xoay màn hình thì biến vẫn được lưu
-            // remember : reset
-//            var count: Int by rememberSaveable { mutableStateOf(0) }
-//            Button(onClick = { count++ }) {
-//                Text(text = "Dem $count", fontSize = 44.sp)
-//            }
+            var catSelected = remember {
+                mutableStateOf<Cat?>(null)
+            }
 
-            // chuyen man hinh : Intent
-            // Chuyen sang 1 man moi hoan toan, ko lien quan man hinh hien tai
-//            Button(onClick = {
-//                val intent = Intent(this, DetailActivity::class.java)
-//                startActivity(intent)
-//            }) {
-//                Text(text = "Open Activity")
-//            }
-            // fragment : view nam tren activity : tab, viewpager, menu drawer
-            val navController = rememberNavController()
-            // NavHost liệt kê các màn hình có trong MainActivity thông qua navController
-            // Mỗi màn hình là 1 composable : được đặt tên (route - địa chỉ)
-            NavHost(navController = navController, startDestination = "login") {
-                composable("login") {
-                    LoginScreen(navController = navController)
+            var listStateCatS = remember {
+                mutableStateListOf(*catList.toTypedArray())
+            }
+
+            LazyColumn {
+                itemsIndexed(listStateCatS) { i, it ->
+                    Column {
+                        Button(onClick = {
+                            catSelected.value = it
+                        }) {
+                            Text(text = "Detail")
+                        }
+                        Button(onClick = {
+                            listStateCatS.remove(it)
+                        }) {
+                            Text(text = "Delete")
+                        }
+                        Text(text = "ID : ${it.id}")
+                        Text(text = "Name : ${it.name}")
+                        Text(text = "Des : ${it.des}")
+                        AsyncImage(
+                            modifier = Modifier.size(90.dp, 90.dp),
+                            model = "${it.url}",
+                            contentDescription = "ABC"
+                        )
+                        Text(text = "-------------------------------------")
+                    }
                 }
-                composable("signup") {
-                    SignUpScreen(navController = navController)
+            }
+            // cu phap lambda de goi gia tri cua mutableState
+            catSelected.value?.let {
+                Dialog(onDismissRequest = { catSelected.value = null }) {
+                    Column {
+                        Text(text = "${catSelected.value?.des}")
+                        Text(text = "${catSelected.value?.name}")
+                        AsyncImage(
+                            modifier = Modifier.size(90.dp, 90.dp),
+                            model = "${it.url}",
+                            contentDescription = "ABC"
+                        )
+                    }
                 }
+//                AlertDialog(
+//                    onDismissRequest = {
+//                        catSelected.value = null
+//                    },
+//                    confirmButton = {
+//                        Button(
+//                            onClick = {
+//                                catSelected.value = null
+//                            }) {
+//                            Text(text = "OK")
+//                        }
+//                    },
+//                    title = { Text(text = "${catSelected.value?.name}") },
+//                    text = { Text(text = "${catSelected.value?.des}") },
+//                )
             }
         }
     }
-    @Composable
-    fun LoginScreen(navController: NavController) {
-        Column {
-            Text(text = "Login", fontSize = 44.sp)
-            Button(onClick = {
-                navController.navigate("signup")
-            }) {
-                Text(text = "Sign Up", fontSize = 22.sp)
-            }
-        }
-    }
-    @Composable
-    fun SignUpScreen(navController: NavController) {
-        Column {
-            Text(text = "Sign Up", fontSize = 44.sp)
-            Button(onClick = {
-                navController.navigate("login")
-            }) {
-                Text(text = "Login", fontSize = 22.sp)
-            }
-        }
-    }
-
-
 }
